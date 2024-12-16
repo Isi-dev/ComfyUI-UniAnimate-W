765e64ae9f73210fa288e176e2b70cbb1f225f55
Isi <isinsemail@gmail.com>
2024-12-16 20:56:36 +0100

    Update __init__.py with Animate_X Scripts

--- tools/inferences/__init__.py
+++ tools/inferences/__init__.py
@@ -1,2 +1,4 @@
 from .inference_unianimate_entrance import *
 from .inference_unianimate_long_entrance import *
+from .inference_animate_x_entrance import *
+from .inference_animate_x_long_entrance import *
