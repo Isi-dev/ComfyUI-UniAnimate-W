309b26aa2a763abc45e3f77fb7f65aa907dd2cf6
Isi <isinsemail@gmail.com>
2024-12-30 22:56:59 +0100

    Update __init__.py
    Included a second version of animate-x node which uses extra data like pose-embeddings

--- tools/inferences/__init__.py
+++ tools/inferences/__init__.py
@@ -2,3 +2,5 @@ from .inference_unianimate_entrance import *
 from .inference_unianimate_long_entrance import *
 from .inference_animate_x_entrance import *
 from .inference_animate_x_long_entrance import *
+from .inference_animate_x_entrance_v2 import *
+
